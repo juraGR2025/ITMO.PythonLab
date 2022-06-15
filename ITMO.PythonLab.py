a12bdea1e948fe538e4471baab4f8fb27c4b83ec
juraGR2025 <93720304+juraGR2025@users.noreply.github.com>
2022-06-15 18:48:46 +0300

    15-06-2022

--- ITMO.PythonLab.py
+++ ITMO.PythonLab.py
@@ -1,1 +1,52 @@
+from random import randint
 
+a,b=int(input('Input first number ')), int(input('Input second number '))
+try:
+    if b==0:
+        raise ZeroDivisionError
+except:
+   print("Деление на 0")
+print("Исключений нет")
+
+name = input('Enter first name ')
+name_2 = input('Enter a second name ')
+try:
+    if len(name) <= 1:
+        raise NameError('HiThere')
+    
+except NameError:
+    print('An exception flew by!')
+    raise
+
+# определение пользовательских исключений
+class Error(Exception):
+    """Базовый класс для других исключений"""
+    pass
+
+class ValueTooSmallError(Error):
+    """Вызывается, когда входное значение мало"""
+    pass
+
+class ValueTooLargeError(Error):
+    """Вызывается, когда входное значение велико"""
+    pass
+
+# число, которое нужно угадать
+number = randint(1, 6)
+
+# игра продолжается до тех пор, 
+# пока пользователь его не угадает
+while True:
+    try:
+        i_num = int(input("Ввести число: "))
+        if i_num < number:
+            raise ValueTooSmallError
+        elif i_num > number:
+            raise ValueTooLargeError
+        break
+    except ValueTooSmallError:
+        print("Это число меньше загаданного, попробуйте еще раз!\n")
+    except ValueTooLargeError:
+        print("Это число больше загаданного, попробуйте еще раз!\n")
+
+print("Поздравляю! Вы правильно угадали.")
